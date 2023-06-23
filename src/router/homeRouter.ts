3f427a4c26fbf9ced4f404ddcf4b7beb1da89284
DragonRF <duonglong410@gmail.com>
2023-06-23 08:13:31 +0700

    add findby methods

--- src/router/homeRouter.ts
+++ src/router/homeRouter.ts
@@ -1,3 +1,4 @@
+// homeRouter.ts
 import {Router} from "express";
 import {adminAuth} from "../middleware/adminAuth";
 import {auth} from "../middleware/auth";
@@ -7,16 +8,16 @@ import {ownerAuth} from "../middleware/owner";
 export const homeRouter = Router();
 
 homeRouter.get("/", homeController.findAll);
-homeRouter.get('/find-by-address', homeController.findHomeByAddress);
-homeRouter.get('/for-rent', homeController.findHomeForRent);
-homeRouter.get('/rented', homeController.findHomeRented);
-homeRouter.post("/", adminAuth, homeController.addHome); // Add adminAuth middleware for admin authority
-homeRouter.delete("/:idHome", adminAuth, homeController.removeHome); // Add adminAuth middleware for admin authority
+homeRouter.get("/find-by-address", homeController.findHomeByAddress);
+homeRouter.get("/for-rent", homeController.findHomeForRent);
+homeRouter.get("/rented", homeController.findHomeRented);
+homeRouter.post("/", adminAuth, homeController.addHome);
+homeRouter.delete("/:idHome", adminAuth, homeController.removeHome);
 homeRouter.get("/filter", homeController.findByNameHome);
 homeRouter.get("/price", homeController.findByPrice);
-homeRouter.put("/:idHome", adminAuth, homeController.editHome); // Add adminAuth middleware for admin authority
+homeRouter.put("/:idHome", adminAuth, homeController.editHome);
 homeRouter.get("/:idHome", homeController.findHomeById);
 homeRouter.get("/categories/:categoryId", homeController.findByCategoryId);
-homeRouter.post("/upload-image/:idHome", adminAuth, homeController.uploadImage); // Add adminAuth middleware for admin authority
-homeRouter.put("/change-status/:idHome", adminAuth, homeController.changeHomeStatus); // Add adminAuth middleware for admin authority
+homeRouter.post("/upload-image/:idHome", adminAuth, homeController.uploadImage);
+homeRouter.put("/change-status/:idHome", adminAuth, homeController.changeHomeStatus);
 homeRouter.get("/status", homeController.findByStatus);
